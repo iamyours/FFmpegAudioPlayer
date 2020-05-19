f3dfa6aba4e33fa2f0aeb0a25a634602171f178f
严星星 <353518643@qq.com>
2020-05-19 17:11:50 +0800

    update readme

--- app/src/main/java/io/github/iamyours/ffmpegaudioplayer/WavePlayerActivity.kt
+++ app/src/main/java/io/github/iamyours/ffmpegaudioplayer/WavePlayerActivity.kt
@@ -3,15 +3,10 @@ package io.github.iamyours.ffmpegaudioplayer
 import android.os.Bundle
 import android.os.Environment
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import kotlinx.android.synthetic.main.activity_wave_player.*
-import java.io.File
-import java.io.FileOutputStream
 
 
 class WavePlayerActivity : AppCompatActivity() {
-    val file = File("/data/data/io.github.iamyours.ffmpegaudioplayer/out.pcm")
-    val fos = FileOutputStream(file)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wave_player)
@@ -25,11 +20,9 @@ class WavePlayerActivity : AppCompatActivity() {
     private var lastTime = System.currentTimeMillis()
     private val list = ArrayList<Byte>()
     private fun updateWave(it: ByteArray) {
-        Log.i("Wave", "size:${it.size}")
         val t = System.currentTimeMillis()
         list.addAll(it.toMutableList())
         if (t - lastTime > 100) {
-            if (list.size < 2000) return
             waveView.post {
                 waveView.update(ByteUtil.byteArray2ShortArray(list))
                 list.clear()
